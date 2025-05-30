c3b2063479f1a25cd7f516fd98b1ac738f053478
rrr <rrr@yandex.by>
2025-05-30 13:41:04 +0300

    router

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route } from "react-router";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Route path="/Me">
+        <App />
+      </Route>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
