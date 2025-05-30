3d83c78328ac48b3b714e6693fe560b9bd22686e
rrr <rrr@yandex.by>
2025-05-30 13:03:18 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SceneViewer from "./components/organisms/SceneViewer";
+import { useShuffle } from "./hooks/useShuffle";
 
 function App() {
+  const {connectors, click}= useShuffle()
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-[100vh] w-[100vw]" onClick={click}>
+      <div className="relative ">
+        <header className="absolute z-50 w-full top-0 left-0 h-[20vh] backdrop-blur">
+          123
+        </header>
+        <main className="absolute top-0 left-0 w-[100vw] h-[100vh]">
+          <SceneViewer connectors={connectors} />
+        </main>
+        <footer>
+          <div className="absolute w-full h-[15vh] top-[85vh] left-0 z-50 backdrop-blur    ">
+            789
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+//https://www.narrowdesign.com/
+//https://www.arrowooddesign.com/
+//https://www.sabanna.online/
---
+++ src/components/organisms/SceneViewer/index.tsx
@@ -0,0 +1,87 @@
+import { Canvas } from "@react-three/fiber";
+import { Physics } from "@react-three/rapier";
+import Pointer from "../Pionter";
+import Connector from "../Connector";
+import Model from "../../models/Model";
+import {
+  Environment,
+  Lightformer,
+  MeshTransmissionMaterial,
+} from "@react-three/drei";
+import { EffectComposer, N8AO } from "@react-three/postprocessing";
+
+export default function SceneViewer(props: any) {
+  return (
+    <Canvas
+      shadows
+      dpr={[1, 1.5]}
+      gl={{ antialias: false }}
+      camera={{ position: [0, 0, 15], fov: 17.5, near: 1, far: 20 }}
+      {...props}
+    >
+      <color attach="background" args={["#141622"]} />
+      <ambientLight intensity={0.4} />
+      <spotLight
+        position={[10, 10, 10]}
+        angle={0.15}
+        penumbra={1}
+        intensity={1}
+        castShadow
+      />
+      <Physics /*debug*/ gravity={[0, 0, 0]}>
+        <Pointer />
+        {
+          props.connectors.map((props: any, i:number) => <Connector key={i} {...props} />) /* prettier-ignore */
+        }
+        <Connector position={[10, 10, 5]}>
+          <Model>
+            <MeshTransmissionMaterial
+              clearcoat={1}
+              thickness={0.1}
+              anisotropicBlur={0.1}
+              chromaticAberration={0.1}
+              samples={8}
+              resolution={512}
+            />
+          </Model>
+        </Connector>
+      </Physics>
+      <EffectComposer enableNormalPass={false} multisampling={8}>
+        <N8AO distanceFalloff={1} aoRadius={1} intensity={4} />
+      </EffectComposer>
+      <Environment resolution={256}>
+        <group rotation={[-Math.PI / 3, 0, 1]}>
+          <Lightformer
+            form="circle"
+            intensity={4}
+            rotation-x={Math.PI / 2}
+            position={[0, 5, -9]}
+            scale={2}
+          />
+          <Lightformer
+            form="circle"
+            intensity={2}
+            rotation-y={Math.PI / 2}
+            position={[-5, 1, -1]}
+            scale={2}
+          />
+          <Lightformer
+            form="circle"
+            intensity={2}
+            rotation-y={Math.PI / 2}
+            position={[-5, -1, -1]}
+            scale={2}
+          />
+          <Lightformer
+            form="circle"
+            intensity={2}
+            rotation-y={-Math.PI / 2}
+            position={[10, 1, 0]}
+            scale={8}
+          />
+        </group>
+      </Environment>
+    </Canvas>
+  );
+}
+
