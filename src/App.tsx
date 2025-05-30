abaf1c3446cd6915ac8d9d776ce91202eb499e17
rrr <rrr@yandex.by>
2025-05-30 13:34:34 +0300

    homepage has changed

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,14 @@ function App() {
     <div className="h-[100vh] w-[100vw]" onClick={click}>
       <div className="relative ">
         <header className="absolute z-50 w-full top-0 left-0 h-[20vh] backdrop-blur">
-          123
+          Here will be header 
         </header>
         <main className="absolute top-0 left-0 w-[100vw] h-[100vh]">
           <SceneViewer connectors={connectors} />
         </main>
         <footer>
           <div className="absolute w-full h-[15vh] top-[85vh] left-0 z-50 backdrop-blur    ">
-            789
+            Here will be footer
           </div>
         </footer>
       </div>
@@ -25,6 +25,3 @@ function App() {
 
 export default App;
 
-//https://www.narrowdesign.com/
-//https://www.arrowooddesign.com/
-//https://www.sabanna.online/
